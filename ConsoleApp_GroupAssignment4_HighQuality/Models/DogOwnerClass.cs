60c84cac57aa8b5942fa6114859f5c4f4c3b8183
Anuhya Evangeline Vijjeswarapu <Avijjeswarapu2970@conestogac.on.ca>
2024-12-04 01:54:10 -0500

    commit Dogownerclass

--- ConsoleApp_GroupAssignment4_HighQuality/Models/DogOwnerClass.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Models/DogOwnerClass.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
-namespace ConsoleApp_GroupAssignment4_HighQuality.Models
+using System.Collections.Generic;
+
+namespace DogWalkingBusiness.Models
 {
-    public class DogOwnerClass
+    public class DogOwner : User
     {
+        public List<Session> ScheduledSessions { get; private set; }
+
+        public DogOwner(string username, string password) : base(username, password)
+        {
+            ScheduledSessions = new List<Session>();
+        }
+
+        public override bool Login(string username, string password)
+        {
+            return Username == username && Password == password;
+        }
     }
 }
+
+
