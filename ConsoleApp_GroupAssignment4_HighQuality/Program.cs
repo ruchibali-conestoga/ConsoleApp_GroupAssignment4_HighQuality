9e6e688cc0efbb94e7731ce446cd7de937291905
Ruchi Bali <Rbali1438@conestogac.on.ca>
2024-12-04 14:03:06 -0500

    save session static (pending dynamic)

--- ConsoleApp_GroupAssignment4_HighQuality/Program.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using ConsoleApp_GroupAssignment4_HighQuality.Models;
 using ConsoleApp_GroupAssignment4_HighQuality.Services;
+using DogWalkingBusiness.Models;
 
 public class Program
 {
@@ -10,6 +11,17 @@ public class Program
         DogWalkingService dogWalkingService = new DogWalkingService();
         FileStorageService fileStorageService = new FileStorageService();
         List<SessionClass> sessions = fileStorageService.loadSessions();
+
+        // dynamic pending:
+        var walker = new DogWalkerClass("walker1", "password", 20.0);
+        var owner = new DogOwnerClass("owner1", "password");
+        var dogName = string.Empty;
+        //
+
+        dogWalkingService.scheduleSession(walker, owner, dogName, DateTime.Now);
+        fileStorageService.saveSessions(sessions); 
+
+        Console.WriteLine("Session scheduled successfully!");
 
     }
 }
