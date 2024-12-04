332f4fba19692097865fe9254e08af4020417d70
Anuhya Evangeline Vijjeswarapu <Avijjeswarapu2970@conestogac.on.ca>
2024-12-04 01:51:16 -0500

    commit userclass

--- ConsoleApp_GroupAssignment4_HighQuality/Models/UserClass.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Models/UserClass.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp_GroupAssignment4_HighQuality.Models
+namespace DogWalkingBusiness.Models
 {
-    public class UserClass
+    public abstract class User
     {
-        // Assign to Anuhya 
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+
+        protected User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public abstract bool Login(string username, string password);
     }
 }
