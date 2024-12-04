2953869e7ccc59732e16dd24d0b0262537c95f51
Ruchi Bali <Rbali1438@conestogac.on.ca>
2024-12-04 08:55:04 -0500

    User class for login - Anuhya

--- ConsoleApp_GroupAssignment4_HighQuality/Models/UserClass.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Models/UserClass.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_GroupAssignment4_HighQuality.Models
 {
-    public class UserClass
+    public abstract class UserClass
     {
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        
+        protected UserClass(string username, string password)
+        {
+             Username = username;
+             Password = password;
+         }
+        
+         public abstract bool Login(string username, string password);
 
     }
 }
