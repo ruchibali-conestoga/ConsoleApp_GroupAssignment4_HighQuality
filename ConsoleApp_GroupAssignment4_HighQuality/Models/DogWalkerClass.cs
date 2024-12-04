6c015e273e9c487c14615170b543f9d503e109db
Ruchi Bali <Rbali1438@conestogac.on.ca>
2024-12-04 09:20:24 -0500

    Dog walker class and session class commit

--- ConsoleApp_GroupAssignment4_HighQuality/Models/DogWalkerClass.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Models/DogWalkerClass.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace ConsoleApp_GroupAssignment4_HighQuality.Models
 {
-    public class DogWalkerClass
+    public class DogWalkerClass: UserClass
     {
+         public double Rate { get; set; }
+         public bool IsAvailable { get; set; }
+         public List<SessionClass> ScheduledSessions { get; private set; }
+         public DogWalkerClass(string username, string password, double rate) : base(username, password)
+         {
+             Rate = rate;
+             IsAvailable = true;
+             ScheduledSessions = new List<SessionClass>();
+        }
+        public override bool Login(string username, string password)
+        {
+            return Username == username && Password == password;
+        }
     }
 }
--- ConsoleApp_GroupAssignment4_HighQuality/Models/SessionClass.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Models/SessionClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace ConsoleApp_GroupAssignment4_HighQuality.Models
 {
     public class SessionClass
     {
+        public DogWalkerClass Walker { get; set; }
+        public DogOwnerClass Owner { get; set; }
+        public string DogName { get; set; }
+        public DateTime Date { get; set; }
+
+        public SessionClass(DogWalkerClass walker, DogOwnerClass owner, string dogName, DateTime date)
+        {
+            Walker = walker;
+            Owner = owner;
+            DogName = dogName;
+            Date = date;
+        }
     }
 }
