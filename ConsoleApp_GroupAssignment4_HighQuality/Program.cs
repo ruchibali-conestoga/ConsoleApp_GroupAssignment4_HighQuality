a8d9220854f5d19a56988b950a3099d4c7c5fb52
Ruchi Bali <Rbali1438@conestogac.on.ca>
2024-12-04 12:09:51 -0500

    service.cs remove and programs.cs add with some little changes

--- ConsoleApp_GroupAssignment4_HighQuality/Program.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Program.cs
@@ -1,7 +1,16 @@
-﻿public class Program
+﻿
+using ConsoleApp_GroupAssignment4_HighQuality.Models;
+using ConsoleApp_GroupAssignment4_HighQuality.Services;
+
+public class Program
 {
     private static void Main(string[] args)
     {
         //Console.WriteLine("Group5 Assignment4!");
+        DogWalkingService dogWalkingService = new DogWalkingService();
+        FileStorageService fileStorageService = new FileStorageService();
+        List<SessionClass> sessions = fileStorageService.loadSessions();
+
     }
 }
+
