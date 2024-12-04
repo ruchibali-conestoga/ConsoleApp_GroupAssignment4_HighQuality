4b1c65cceb8be765de7fdea187eea024be17c8c9
Anuhya Evangeline Vijjeswarapu <Avijjeswarapu2970@conestogac.on.ca>
2024-12-04 01:53:33 -0500

    commit dogWalikungSerivices

--- ConsoleApp_GroupAssignment4_HighQuality/Interface/IDogWalkingService.cs
+++ ConsoleApp_GroupAssignment4_HighQuality/Interface/IDogWalkingService.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
-namespace ConsoleApp_GroupAssignment4_HighQuality.Interface
-{
+using System.Collections.Generic;
+
+namespace DogWalkingBusiness.Models
+ {
     public interface IDogWalkingService
     {
+        void ScheduleSession(DogWalker walker, DogOwner owner, string dogName, DateTime date);
+        List<Session> GetSessionsForWalker(DogWalker walker);
+        List<Session> GetSessionsForOwner(DogOwner owner);
+        List<Session> GetAllSessions();
     }
-}
+   }
+
+
